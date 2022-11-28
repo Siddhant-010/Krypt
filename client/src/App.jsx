78e6630a5829f533a3357b7f9677c666f44c8d2b
Siddhant Jain <jsiddhant010@gmail.com>
2022-11-28 11:32:16 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import {
+  Navbar,
+  Welcome,
+  Transactions,
+  Services,
+  Loader,
+  Footer,
+} from "./components";
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.API_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
